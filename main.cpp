e5c494a2edb73175ba6374b7a3313c070e41c5e9
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-11-10 18:54:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
     int a=0, primo_numero=0, secondo_numero=1, serie=0;
     cin >> a;
 
+    if ( a>=2 ) {
         cout << secondo_numero << endl;
         for (int i=2; i <=a; i++) {
             serie = primo_numero + secondo_numero;
